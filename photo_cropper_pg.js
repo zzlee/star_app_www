b7c027ef72fbee2fe7fcee5669b88cd6ca9803ba
gzhu <gance@feltmeng.com>
2013-04-29 18:44:03 +0800

    submit button implemented, but not well debugged

--- photo_cropper_pg.js
+++ photo_cropper_pg.js
@@ -48,8 +48,133 @@ load: function(event, data){
     image.src = fileProcessedForCropperURI;
     
 
-    $('#btnUseCamera').bind( "click", function(){
+    var onSubmitBtnClick= function() {
+        var uploadFail_cb = function(error) {
+            
+            var onConfirm = function(buttonIndex) {
+                switch(buttonIndex) {
+                    case 1:
+                        $('#submitPhotoBtn').click();
+                        break;
+                    case 2:
+                        $.mobile.changePage("my_video.html");
+                        break;
+                }
+            }
+            navigator.notification.confirm(
+                                           '喔喔！上傳失敗了。您的網路OK嗎？',  // message
+                                           onConfirm,              // callback to invoke with index of button pressed
+                                           ' ',            // title
+                                           '重新上傳,以後再玩'          // buttonLabels
+                                           );
+            
+        }
         
+        
+        var uploadSuccess_cb = function(r) {
+            console.log("Code = " + r.responseCode);
+            console.log("Response = " + r.response);
+            console.log("Sent = " + r.bytesSent);
+            
+            //$('#divStatus').html("檔案上傳成功！");
+            
+            customizedContent.customizableObjects[0].content = fileSelected;
+            
+            
+            customizedContent.timeStamp = (new Date()).toISOString();
+            
+            
+            $.post(starServerURL+'/miix/videos/user_content_description', customizedContent, function(result){
+                   console.dir("upload user data info result: "+result);
+                   if ( !result.err ) {
+                   FmMobile.addProcessingWork(projectID);
+                   $.mobile.hidePageLoadingMsg();
+                   $.mobile.changePage("my_video.html");
+                   
+                   }
+                   })
+            .error(function() {
+                   var onConfirm = function(buttonIndex) {
+                   switch(buttonIndex) {
+                   case 1:
+                   $('#submitPhotoBtn').click();
+                   break;
+                   case 2:
+                   $.mobile.changePage("my_video.html");
+                   break;
+                   }
+                   }
+                   navigator.notification.confirm(
+                                                  '喔喔！上傳失敗了。您的網路OK嗎？',  // message
+                                                  onConfirm,              // callback to invoke with index of button pressed
+                                                  ' ',            // title
+                                                  '重新上傳,以後再玩'          // buttonLabels
+                                                  );
+                   });
+        }
+        
+        
+        var uploadPhoto = function (imageURI) {
+            var options = new FileUploadOptions();
+            options.fileKey = "file";
+            options.fileName = imageURI.substr(imageURI.lastIndexOf('/')+1);
+            fileSelected = options.fileName;
+            options.mimeType = "image/jpeg";
+            //options.mimeType = "image/png";
+            options.chunkedMode = true;
+                        
+            var params = new Object();
+            params.fileObjectID = fileObjectID;
+            params.projectID = projectID;
+            params.croppedArea_x = croppedArea.x;
+            params.croppedArea_y = croppedArea.y;
+            params.croppedArea_width = croppedArea.width;
+            params.croppedArea_height = croppedArea.height;
+            params.obj_OriginalWidth = customizableObjectDimensions[fileObjectID].width;
+            params.obj_OriginalHeight = customizableObjectDimensions[fileObjectID].height;
+            params.osVersion = "iOS_"+device.version;
+            
+            
+            console.log('[uploadPhoto()] params.projectID = %s', params.projectID);
+            console.log('[uploadPhoto()] params.fileObjectID = %s', params.fileObjectID);
+            
+            
+            options.params = params;
+            options.chunkedMode = true;
+            
+            var ft = new FileTransfer();
+            
+            ft.onprogress = function(progressEvent) {
+                if (progressEvent.lengthComputable) {
+                    var uploadPercentage = progressEvent.loaded / progressEvent.total * 100;
+                    console.log("uploadPercentage=" + uploadPercentage.toString());
+                } else {
+                    console.log("upload some chunk....");
+                }
+            };
+            
+            
+            ft.upload(imageURI, starServerURL+"/miix/videos/user_content_files", uploadSuccess_cb, uploadFail_cb, options);
+            
+            $("#moviePreview").trigger("stopPreviewAnimation");
+            $('#divStatus').html("照片上傳成功後即開始影像合成，合成完成後會通知您去分享給朋友！");
+            $('#divStatus').show();
+            $('#photoSubmitButtons').hide();
+            $.mobile.showPageLoadingMsg();
+            
+        }
+        
+        uploadPhoto(fileSelectedURI);
+        FmMobile.analysis.trackEvent("Button", "Click", "Submit", 24);
+        //recordUserAction("submits a Photo");
+        
+        recordUserAction("submits a Photo", true);
+        
+    };
+    
+    $('#submitBtn').click(onSubmitBtnClick);
+    $('#cancelBtn').click(function(){
+        $.mobile.changePage("movie_create.html");
     });
     
 },
