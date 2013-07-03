2f54582bb83f918b2dbac213a0eeda99f19b2065
joy <joy@feltmeng.com>
2013-07-03 12:15:53 +0800

    joy test

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-var starServerURL = "http://www.feltmeng.idv.tw";
-//var starServerURL = 'http://192.168.5.109:3000';
+//var starServerURL = "http://www.feltmeng.idv.tw";
+var starServerURL = 'http://192.168.5.129';
 //var starServerURL = "http://www.miix.tv";
 
