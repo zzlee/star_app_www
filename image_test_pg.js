6398227d3ce2cd3c0806876b2b5d5181b469a552
gzhu <gance@feltmeng.com>
2013-07-17 12:05:52 +0800

    added CanvasText lib

--- image_test_pg.js
+++ image_test_pg.js
@@ -50,7 +50,11 @@ FmMobile.imageTestPg = {
             
         
         };
-        
+ 
+                // Draw it!
+            
+ 
+
         
         //var resultCanvas = document.getElementById('resultCanvas');
         
@@ -80,6 +84,61 @@ FmMobile.imageTestPg = {
                 ctx.drawImage(bgImage,0,0);
                 //ctx.translate(742,19);
                 
+                //== CanvasText ==
+                // Definition of global vars
+                var Canvas,Context;
+                // Creation of the new CanvasText instance.
+                var CT = new CanvasText;
+
+                Canvas = ugcCanvas;
+                Context = ctx;
+
+                // Once window is loaded we set the configuration and the default styles.
+                CT.config({
+                    canvas: Canvas,
+                    context: Context,
+                    fontFamily: "Verdana",
+                    fontSize: "14px",
+                    fontWeight: "normal",
+                    fontColor: "#000",
+                    lineHeight: "24"
+                });
+                /*
+                 * Definition of some style classes.
+                 */
+                CT.defineClass("blue",{
+                    fontSize: "22px",
+                    fontColor: "#29a1f1",
+                    fontFamily: "Impact",
+                    fontWeight: "normal",
+                    textShadow: "2px 2px 2px #919191"
+                });
+
+                CT.defineClass("pink",{
+                    fontSize: "22px",
+                    fontColor: "#ff5e99",
+                    fontFamily: "Times new roman",
+                    fontWeight: "bold",
+                    fontStyle: "italic"
+                });
+                //== end of CanvasText ==
+                
+                var drawText = function(x, y, boxWidth, lineHeight) {
+                	
+                }
+                
+
+                // The text we want to draw.
+                var text = 'In this example I don\'t care about <class="blue">blue</class> or <class="pink">pink</class>.\n\
+                            I just care about the new and <class="blue">exciting</class> <class="pink">automatic</class>\n\
+                            <class="blue">multiline</class> feature!! <br />Just to be sure:<br />Lorem ipsum dolor sit amet, \n\
+                            consectetur adipiscing elit. Nulla ut erat magna, quis commodo nulla.\n\
+                            Vestibulum <class="pink">eget</class> mi quis sapien lacinia porta eget eget neque. Aliquam lacus \n\
+                            leo, sodales sit amet laoreet non, mollis ut nibh.';
+
+                var textChinse = '很多人習慣在手機充電時講電話，這樣的動作千萬得小心，大陸南方航空一名空姐，11日晚間在家中用充電的iPhone5講電話，沒想到一股電流通過身體，造成空姐當場死亡';
+
+                
                 ctx.save();
                 ctx.translate(775,22);
                 ctx.rotate(10.36*Math.PI/180);
@@ -90,6 +149,19 @@ FmMobile.imageTestPg = {
                 ctx.translate(885,22);
                 ctx.drawImage(objImage,0,0,240, 180);
                 ctx.restore();
+
+                ctx.save();
+                ctx.translate(775,22);
+                ctx.rotate(10.36*Math.PI/180);
+                //draw text
+                CT.drawText({
+                    text:textChinse,
+                    x: 0,
+                    y: 0,
+                    boxWidth:240
+                });
+                ctx.restore();
+                
                 
                 $("#canvasImg").attr("src", ugcCanvas.toDataURL('image/png'));
                 
