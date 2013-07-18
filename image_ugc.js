cfd24f35062bc7c35f6e75a3cfd86d0c57b32fd9
gzhu <gance@feltmeng.com>
2013-07-19 00:17:55 +0800

    Can upload image UGC to server.

--- image_ugc.js
+++ image_ugc.js
@@ -83,15 +83,16 @@ ImageUgc = (function(){
 			uploadToServer:function(ugcProjectId, ugcInfo, cbOfUploadToServer){
 				var reultURI = ugcCanvas.toDataURL('image/png').replace('image/octet-stream');
                 
-				$.ajax( starServerURL+"/miix/base64_image_ugcs/123456", {
-                //$.ajax( starServerURL+"/miix/base64_image_ugcs/"+ugcProjectId, {
+				 
+                $.ajax( starServerURL+"/miix/base64_image_ugcs/"+ugcProjectId, {
 					type: "PUT",
                     data: {
                         imgBase64: reultURI,
                         ownerId: ugcInfo.ownerId._id,
                         ownerFbUserId: ugcInfo.ownerId.fbUserId,
                         contentGenre: ugcInfo.contentGenre,
-                        title: ugcInfo.title
+                        title: ugcInfo.title,
+                        time: (new Date()).getTime()
                     },
                     success: function(data, textStatus, jqXHR ){
 						if (cbOfUploadToServer){
