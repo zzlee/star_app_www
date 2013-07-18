507ec0b00685905b7b4d490fc3873dfd492d92c6
Jean <jean@feltmeng.com>
2013-07-18 15:30:22 +0800

    update CSS and remove nab-bar  from fb_login_pg

--- fb_login_pg.js
+++ fb_login_pg.js
@@ -5,11 +5,13 @@ FmMobile.fbLoginPg = {
         $("#go_fb_login").click(function(){
             FmMobile.authPopup.init();
         });
+        
+        $('#nav-bar').hide();
     },
 
     show: function(){
         FmMobile.analysis.trackPage("/fbLoginPg");
-        recordUserAction("enters fbLoginPg");
+//        recordUserAction("enters fbLoginPg");
     }
 };
 
