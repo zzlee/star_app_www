96b4de69851c798725f23e3d80bcf895c5f605bf
joy <joy@feltmeng.com>
2013-07-22 15:02:58 +0800

    fixed QA

--- common.js
+++ common.js
@@ -1,4 +1,5 @@
 var starServerURL = "http://www.feltmeng.idv.tw";
 //var starServerURL = 'http://192.168.5.129:80';
 //var starServerURL = "http://www.miix.tv";
+//var starServerURL = "http://192.168.5.118:80";
 
--- setting-customer_pg.js
+++ setting-customer_pg.js
@@ -44,17 +44,17 @@ FmMobile.customerQuestionPg = {
         //
         
         $.get(starServerURL+"/miix_service/"+memberId+"/questions",{token:"53768608"},function(data,status){
-              alert(status);
-              alert(data[1].content.question);
+              alert("get"+status);
+              //alert(data[1].question);
               console.log(data);
-              alert(data[1].content.questionTime);
+              //alert(data[1].questionTime);
               
               $("#userQuesiotns").html('');
               
               
-              for(var i=0;i<data.length;i++){
+              for(var i=0;i<data.message.length;i++){
               
-                          var _d= new Date(Number(data[i].content.questionTime));
+                          var _d= new Date((data.message[i].questionTime));
                           var _y=_d.getFullYear();
                           var _m=_d.getMonth()+1;
                           var dayOfmonth=_d.getDate();
@@ -62,7 +62,7 @@ FmMobile.customerQuestionPg = {
                           var minute=_d.getMinutes();
                           var timeOutput_q=_y+"年"+_m+"月"+dayOfmonth+"號"+" "+hour+":"+minute;
                           
-                          var a_d= new Date(Number(data[i].content.answerTime));
+                          var a_d= new Date((data.message[i].answerTime));
                           var a_y=a_d.getFullYear();
                           var a_m=a_d.getMonth()+1;
                           var a_dayOfmonth=a_d.getDate();
@@ -70,12 +70,12 @@ FmMobile.customerQuestionPg = {
                           var a_minute=a_d.getMinutes();
                           var a_timeOutput=a_y+"年"+a_m+"月"+a_dayOfmonth+"號"+" "+a_hour+":"+a_minute;
               
-              
+              console.log(data.message[i].question);
 
                   $("#userQuesiotns").append("<div class='question'>Question : "+
-                                      data[i].content.question+"<br>"+"<div class='time'>"+
+                                      data.message[i].question+"<br>"+"<div class='time'>"+
                                       timeOutput_q+"</div>"+"<br></div><div class='answer'>"+"Ans : "+
-                                      data[i].content.answer+"<br>"+"<div class='time'>"+
+                                      data.message[i].answer+"<br>"+"<div class='time'>"+
                                       a_timeOutput+"</div>"+"<br></div>"+
                                       "<hr>");
               
