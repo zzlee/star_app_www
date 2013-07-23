232190da38555a136631cebcd10c849c372ac3ff
gzhu <gance@feltmeng.com>
2013-07-23 16:10:22 +0800

    temp version: upload user content to server (not 100% done)

--- image_ugc.js
+++ image_ugc.js
@@ -83,28 +83,86 @@ ImageUgc = (function(){
 			uploadToServer:function(ugcProjectId, ugcInfo, cbOfUploadToServer){
 				var reultURI = ugcCanvas.toDataURL('image/png').replace('image/octet-stream');
                 
-				 
-                $.ajax( starServerURL+"/miix/base64_image_ugcs/"+ugcProjectId, {
-					type: "PUT",
-                    data: {
-                        imgBase64: reultURI,
-                        ownerId: ugcInfo.ownerId._id,
-                        ownerFbUserId: ugcInfo.ownerId.fbUserId,
-                        contentGenre: ugcInfo.contentGenre,
-                        title: ugcInfo.title,
-                        time: (new Date()).getTime()
-                    },
-                    success: function(data, textStatus, jqXHR ){
-						if (cbOfUploadToServer){
-							cbOfUploadToServer(null, data);
-						}
-					},
-					error: function(jqXHR, textStatus, errorThrown){
-						if (cbOfUploadToServer){
-							cbOfUploadToServer(errorThrown, null);
-						}
-					}
-                });
+				async.series([
+				    function(callback){
+				        //upload result image UGC to server
+				        $.ajax( starServerURL+"/miix/base64_image_ugcs/"+ugcProjectId, {
+		                    type: "PUT",
+		                    data: {
+		                        imgBase64: reultURI,
+		                        ownerId: ugcInfo.ownerId._id,
+		                        ownerFbUserId: ugcInfo.ownerId.fbUserId,
+		                        contentGenre: ugcInfo.contentGenre,
+		                        title: ugcInfo.title,
+		                        time: (new Date()).getTime()
+		                    },
+		                    success: function(data, textStatus, jqXHR ){
+		                        callback(null);
+		                    },
+		                    error: function(jqXHR, textStatus, errorThrown){
+		                        callback("Failed to upload image UGC to server: "+errorThrown);
+		                    }
+		                });
+				    },
+                    function(callback){
+				        //upload original user content to server
+				        var imageUri = userContent.picture.urlOfOriginal;
+                        var options = new FileUploadOptions();
+                        options.fileKey = "file";
+                        options.fileName = imageUri.substr(imageUri.lastIndexOf('/')+1);
+                        options.mimeType = "image/jpeg"; //TODO: to have mimeType customizable? 
+                        options.chunkedMode = true;
+                        
+                        fileSelected = options.fileName;
+                        var fileObjectID = customizableObjects[0].ID; //TODO: hard code for now. Any better way?
+                        
+                        var params = new Object();
+                        //params.fileObjectID = fileObjectID; //not used in server side
+                        params.projectID = ugcProjectId; //for server side to save user content to specific project folder
+                        //for server side to crop the user content image
+                        params.croppedArea_x = userContent.picture.crop._x;
+                        params.croppedArea_y = userContent.picture.crop._y;
+                        params.croppedArea_width = userContent.picture.crop._w;
+                        params.croppedArea_height = userContent.picture.crop._h;
+                        //for server side to zoom the user content image to the same size as original footage image
+                        params.obj_OriginalWidth = customizableObjectDimensions[fileObjectID].width;
+                        params.obj_OriginalHeight = customizableObjectDimensions[fileObjectID].height;
+                        
+                        options.params = params;
+                        options.chunkedMode = true;
+                        
+                        var ft = new FileTransfer();
+                        
+                        ft.onprogress = function(progressEvent) {
+                            if (progressEvent.lengthComputable) {
+                                var uploadPercentage = progressEvent.loaded / progressEvent.total * 100;
+                                console.log("uploadPercentage=" + uploadPercentage.toString());
+                            } else {
+                                console.log("upload some chunk....");
+                            }
+                        };
+                        
+                        var uploadSuccess_cb = function(r) {
+                            callback(null);
+                        };
+                        
+                        var uploadFail_cb = function(error) {
+                            console.log("upload error source " + error.source);
+                            console.log("upload error target " + error.target);
+                            callback("Failed to uplaod user content file to server: "+error.code);
+                        };
+                        
+                        ft.upload(imageUri, starServerURL+"/miix/videos/user_content_files", uploadSuccess_cb, uploadFail_cb, options);
+
+                    }
+			    ],
+		        function(err, results){
+				    if (cbOfUploadToServer){
+                        cbOfUploadToServer(err, results);
+                    }
+		        });
+				
+                
 			}
 		};
 		
