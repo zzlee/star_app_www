5caa542d5c36a7489af8be459a104fb27ce1b62e
Jean <jean@feltmeng.com>
2013-08-06 13:08:24 +0800

    fixed live content on my_ugc

--- my_ugc_pg.js
+++ my_ugc_pg.js
@@ -218,6 +218,35 @@ test: function(arry){
                     }
                     widget.appendTo(parent);
                     break;
+                case "miix_image_live_photo":
+                    var projectId = arryContents[i].ProjectId;
+                    console.log("s3 :" + arryContents[i].Url.s3);
+                    var s3Url = arryContents[i].Url.s3;
+                    this.imageThumbnail = $("<img>").attr({
+                                                          id: "imgS3_" +projectId,
+                                                          src: "https://s3.amazonaws.com/miix_content" + s3Url,
+                                                          class: "content-movie-img"
+                                                          });
+                    this.imageThumbnail.appendTo(widget);
+                    
+                    this.shareYoutubeDiv = $("<img>").attr({
+                                                           id: "copyUrlS3_" + projectId,
+                                                           class: "share",
+                                                           src: "images/youtube.png"
+                                                           });
+                    this.shareYoutubeDiv.appendTo(info);
+                    
+                    this.shareFbDiv = $("<img>").attr({
+                                                      id: "shareFb_" + projectId,
+                                                      class: "share",
+                                                      src: "images/facebook.png"
+                                                      });
+                    this.shareFbDiv.appendTo(info);
+                    info.appendTo(widget);
+                    
+                    widget.appendTo(parent);
+                    break;
+
                 default :
                     console.log("Eroor : no Genre");
             }
