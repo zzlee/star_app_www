014f5d4c34d388bfda99b5113a77ba12e56c2d71
joy <joy@feltmeng.com>
2013-08-15 16:18:15 +0800

    fixed check text

--- setting_about_pg.js
+++ setting_about_pg.js
@@ -8,14 +8,7 @@ show: function(){
     
 init: function(){
     $('#nav-bar').show();
-    $('#contentAboutMiixIt').click(function(){
-                                   var ref=window.open('http://apache.org', '_blank', 'location=yes');
-
-                                   
-                                   ref.addEventListener('loadstart', function() { alert(event.url); });
-                                   
-                                                                      });
-       $("#back_setting").click(function(){
+           $("#back_setting").click(function(){
                              $.mobile.changePage("setting-main.html");
                              });
     
