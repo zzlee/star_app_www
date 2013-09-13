c9806d3f768efe95aa609150bb07dc4a3a8f63cf
joy <joy@feltmeng.com>
2013-09-13 13:02:31 +0800

    change right ip

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-//var starServerURL = "http://www.ondascreen.com";
+var starServerURL = "http://www.ondascreen.com";
 //var starServerURL = "http://www.feltmeng.idv.tw";
 //var starServerURL = "http://176.34.55.31";
 //var starServerURL = "http://192.168.5.124";
@@ -6,5 +6,5 @@
 //var starServerURL = 'http://192.168.5.129:80';
 //var starServerURL = "http://www.miix.tv";
 //var starServerURL = "http://192.168.5.118";
-var starServerURL = "http://192.168.5.105";
+//var starServerURL = "http://192.168.5.105";
 
