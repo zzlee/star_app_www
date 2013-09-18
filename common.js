8311a8e5e4cb1358a3267a288397233e13d1cf55
joy <joy@feltmeng.com>
2013-09-18 17:34:23 +0800

    prepared to upload app store

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
-//var starServerURL = "http://www.ondascreen.com";
-var starServerURL = "http://www.feltmeng.idv.tw";
+var starServerURL = "http://www.ondascreen.com";
+//var starServerURL = "http://www.feltmeng.idv.tw";
 //var starServerURL = "http://176.34.55.31";
 //var starServerURL = "http://192.168.5.124";
 //var starServerURL = "http://192.168.5.118";
